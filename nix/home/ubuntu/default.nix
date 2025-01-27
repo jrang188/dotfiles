76db610240cdd8bf6ec6e5661bbf4e995c7a84e6
Justin Ang <justinang177@gmail.com>
2025-01-26 21:51:43 -0800

    add shell alias and set git to use ssh.exe for ubuntu wsl

--- nix/home/ubuntu/default.nix
+++ nix/home/ubuntu/default.nix
@@ -14,4 +14,16 @@
   ];
 
   nixpkgs.config.allowUnfree = true;
+  programs.git = {
+    extraConfig = {
+      core.sshCommand = "ssh.exe";
+    };
+  };
+
+  # Use .exe if using WSL
+  shellAliases = {
+    ssh = "ssh.exe";
+    ssh-add = "ssh-add.exe";
+  };
+
 }

3fc960be48d8d72a0af38aec769672dabfd80b73
Justin Ang <justinang177@gmail.com>
2025-02-09 01:26:03 -0800

    ensure ubuntu wsl also have the hack for cord.nvim

--- nix/home/ubuntu/default.nix
+++ nix/home/ubuntu/default.nix
@@ -7,6 +7,22 @@
       ssh = "ssh.exe";
       ssh-add = "ssh-add.exe";
     };
+    initExtra = ''
+      nvim() {
+          if ! pidof socat > /dev/null 2>&1; then
+              [ -e /tmp/discord-ipc-0 ] && rm -f /tmp/discord-ipc-0
+              socat UNIX-LISTEN:/tmp/discord-ipc-0,fork EXEC:"/mnt/c/bin/npiperelay.exe //./pipe/discord-ipc-0" &
+          fi
+
+          if [ $# -eq 0 ]; then
+              command nvim
+          else
+              command nvim "$@"
+          fi
+      }
+
+      vim() { nvim "$@"; }
+    '';
   };
 
   home.packages = with pkgs; [
@@ -16,6 +32,7 @@
     ffmpeg
     wget
     curl
+    socat
   ];
 
   nixpkgs.config.allowUnfree = true;

4a7f5edf3a74e18e582704aba7467e6fcc6b72fa
Justin Ang <justinang177@gmail.com>
2025-02-15 19:54:19 -0800

    ensure certain configs only load when not using vscode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,9 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("fzf-lua").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+if not vim.g.vscode then
+  vim.keymap.set(
+    "n",
+    "<leader>sx",
+    require("fzf-lua").resume,
+    { noremap = true, silent = true, desc = "Resume" }
+  )
+end

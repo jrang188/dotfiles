ebfb60c99c68e2e0f3192a3904fae2cdfeb68d4b
Justin Ang <justinang177@gmail.com>
2025-03-13 00:33:27 -0700

    change to tokyonight and add jankyborders

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-  -- add
-  { "catppuccin/nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "tokyonight-night",
     },
   },
 }

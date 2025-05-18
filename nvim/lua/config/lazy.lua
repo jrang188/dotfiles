78ac19a4cc499dd6307b855a42f6545e72ffc710
Justin Ang <justinang177@gmail.com>
2025-05-17 19:34:14 -0700

    remove mini-animate

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -3,8 +3,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({ { "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" },
-      { "\nPress any key to exit..." } }, true, {})
+    vim.api.nvim_echo(
+      { { "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" }, { "\nPress any key to exit..." } },
+      true,
+      {}
+    )
     vim.fn.getchar()
     os.exit(1)
   end
@@ -14,13 +17,10 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",                                import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- Extras
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "plugins" }
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -28,23 +28,28 @@ require("lazy").setup({
     lazy = false,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
-    version = false -- always use the latest git commit
+    version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = {
-    colorscheme = { "tokyonight", "habamax" }
+    colorscheme = { "tokyonight", "habamax" },
   },
   checker = {
     enabled = true, -- check for plugin updates periodically
-    notify = false  -- notify on update
-  },                -- automatically check for plugin updates
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
-      disabled_plugins = { "gzip", -- "matchit",
+      disabled_plugins = {
+        "gzip", -- "matchit",
         -- "matchparen",
         -- "netrwPlugin",
-        "tarPlugin", "tohtml", "tutor", "zipPlugin" }
-    }
-  }
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
 })

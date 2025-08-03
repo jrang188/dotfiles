64c66d5001c75b81a629c2c40f5177edba6dd90b
Justin Ang <justinang177@gmail.com>
2025-08-03 11:40:44 -0700

    more cleanup

--- nix/modules/darwin/system.nix
+++ nix/modules/darwin/system.nix
@@ -45,9 +45,5 @@
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  # this is required if you want to use darwin's default shell - zsh
-  programs.zsh.enable = true;
-
   fonts.packages = with pkgs-unstable; [ sketchybar-app-font ];
 }

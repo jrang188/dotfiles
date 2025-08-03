4ff7cf1359a91bb02a213375f0c953b3db6011c6
Justin Ang <justinang177@gmail.com>
2025-08-03 14:36:45 -0700

    add polkit integration to allow 1pass system auth

--- nix/modules/nixos/1password.nix
+++ nix/modules/nixos/1password.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ username, ... }:
 {
   programs = {
-    _1password-gui.enable = true;
+    _1password-gui = {
+      enable = true;
+      # Certain features, including CLI integration and system authentication support,
+      # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+      polkitPolicyOwners = [ username ];
+    };
     _1password.enable = true;
   };
 }

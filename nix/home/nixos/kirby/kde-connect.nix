96fdfb2385944b1eb99d505128f9b35a126d1c5b
Justin Ang <justinang177@gmail.com>
2025-08-03 17:39:19 -0700

    add kde connect

---
+++ nix/home/nixos/kirby/kde-connect.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  services.kdeconnect = {
+    enable = true;
+    indicator = true;
+  };
+
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+}

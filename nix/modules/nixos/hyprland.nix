997aaf88ee45a5722ab24c0b773c38a4a031cb13
Justin Ang <justinang177@gmail.com>
2025-08-30 23:53:30 -0700

    following vimjoyer tutorial

--- nix/modules/nixos/hyprland.nix
+++ nix/modules/nixos/hyprland.nix
@@ -3,6 +3,27 @@
   programs.hyprland = {
     enable = true;
     package = pkgs-unstable.hyprland;
+    xwayland.enable = true;
     portalPackage = pkgs-unstable.xdg-desktop-portal-hyprland;
   };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  environment.systemPackages = with pkgs-unstable; [
+    (warbar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
+
+    # Notification Daemons
+    mako
+    libnotify
+
+    # wallpaper
+    swww
+
+    # app launcher
+    rofi-wayland
+  ];
 }

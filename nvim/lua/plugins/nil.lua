01cfec5e40058de550b3b1426f505ea82982b87e
Justin Ang <justinang177@gmail.com>
2026-02-01 21:06:46 -0800

    use treefmt for now for nix neovim fmt

--- nvim/lua/plugins/nil.lua
+++ nvim/lua/plugins/nil.lua
@@ -6,7 +6,7 @@ return {
         settings = {
           ["nil"] = {
             formatting = {
-              command = { "nixfmt" },
+              command = { "treefmt" },
             },
             nix = {
               flake = {

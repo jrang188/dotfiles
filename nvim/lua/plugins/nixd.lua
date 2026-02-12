62a4561b4a896d8e61fbcbc9af36d4329c46a694
Justin Ang <justinang177@gmail.com>
2026-02-11 23:57:42 -0800

    nixd remove hardcode

--- nvim/lua/plugins/nixd.lua
+++ nvim/lua/plugins/nixd.lua
@@ -1,3 +1,44 @@
+local flake_path = vim.fn.expand("~/dotfiles/nix")
+local hostname = (vim.loop.os_gethostname() or ""):gsub("%..*$", "")
+local username = vim.env.USER or "sirwayne"
+local is_darwin = (vim.loop.os_uname().sysname or "") == "Darwin"
+
+local system_options_expr = string.format(
+  [[
+let
+  flake = builtins.getFlake "%s";
+  configs = flake.%s;
+in
+  if builtins.hasAttr "%s" configs
+  then configs."%s".options
+  else (builtins.head (builtins.attrValues configs)).options
+]],
+  flake_path,
+  is_darwin and "darwinConfigurations" or "nixosConfigurations",
+  hostname,
+  hostname
+)
+
+local home_options_expr = string.format(
+  [[
+let
+  flake = builtins.getFlake "%s";
+  configs = flake.homeConfigurations;
+  user_host = "%s@%s";
+in
+  if builtins.hasAttr user_host configs
+  then configs.${user_host}.options
+  else if builtins.hasAttr "%s" configs
+  then configs."%s".options
+  else (builtins.head (builtins.attrValues configs)).options
+]],
+  flake_path,
+  username,
+  hostname,
+  username,
+  username
+)
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -12,11 +53,11 @@ return {
               expr = "import <nixpkgs> {}",
             },
             options = {
-              nixos = {
-                expr = "(builtins.getFlake \"/home/sirwayne/dotfiles/nix\").nixosConfigurations.kirby.options",
+              system = {
+                expr = system_options_expr,
               },
               home_manager = {
-                expr = "(builtins.getFlake \"/home/sirwayne/dotfiles/nix\").homeConfigurations.\"sirwayne@kirby\".options",
+                expr = home_options_expr,
               },
             },
           },

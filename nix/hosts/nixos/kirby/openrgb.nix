ea9e3064ab7d5053ca9e8611d10f90d861744029
Justin Ang <justinang177@gmail.com>
2026-02-14 17:08:36 -0800

    add uni-sync configuration

--- nix/hosts/nixos/kirby/openrgb.nix
+++ nix/hosts/nixos/kirby/openrgb.nix
@@ -3,9 +3,32 @@
   services.hardware.openrgb = {
     enable = true;
     motherboard = "amd";
+    package = pkgs.openrgb-with-all-plugins;
   };
-
-  environment.systemPackages = with pkgs; [
-    openrgb-with-all-plugins
-  ];
+  hardware = {
+    # Enable UniSync for OpenRGB (Control LianLi SL Fans)
+    uni-sync = {
+      enable = true;
+      devices = [
+        {
+          device_id = "VID=3314/PID=41216/SN=6243168001/PATH=1-7.1=1.1";
+          sync_rgb = true;
+          channels = [
+            {
+              mode = "PWM";
+            }
+            {
+              mode = "PWM";
+            }
+            {
+              mode = "PWM";
+            }
+            {
+              mode = "PWM";
+            }
+          ];
+        }
+      ];
+    };
+  };
 }
